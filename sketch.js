32a4a9d8dc191411d1b89e1d7f1ea08ba842965d
uzin0502 <uzin0502@naver.com>
2020-04-20 22:25:54 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//YAY!
 let apeshit;
 let dino1;
 let dino2;
@@ -31,68 +32,69 @@ function draw() {
   background(mouseX, mouseY, 100);
   colorMode(RGB, 1);
   // set light
-  ambientLight(0.2); //La
-  specularColor(1, 1, 1); // Ks
+  ambientLight(0.2);
+  specularColor(1, 1, 1);
   directionalLight(1, 1, 1, 0, 0, -1);
   // Ld and Ls
   // let mouse X control rotation
   let r = map(mouseX, 0, width, -radians(180), radians(180));
   rotateY(r);
-  // display mona lisa
-  //noStroke();
-  //beginShape();
-  //texture(mona);
-  //plane(457, 600, 100, 100);
-  //endShape();
-  // let mouse Y control color hue
   let h = map(mouseY, 0, height, 0, 360);
   colorMode(HSB, 360, 100, 100, 100);
   // material setting
   fill(h, 100, 100, 60);
-  // lambert only model, Kd
-  // try the following blinn-phong model and see how it changes
-  // specularMaterial(h,100,100, 60);
-  // parameters set both Ka and Kd
-  // shininess(10);
-  // shininess of specular region, ns
   // display a glass box
   stroke(0, 0, 100);
   box(305, 455, 200);
   //dino2
   scale(200.0);
-  //rotateY(frameCount * 0.1);
-  //rotateZ(frameCount * 0.3);
-  translate(0.3,0,0);
+  rotateY(frameCount * mouseY * 0.0001);
+  rotateZ(frameCount * mouseX * 0.0001);
+  translate(0.3, 0, 0);
   model(DINO);
-  translate(-0.3,0.1,0);
+  translate(-0.3, 0.1, 0);
   model(DINOO);
-  translate(0.2,0.2,0);
+  translate(0.2, 0.2, 0);
   model(DINOOO);
-  translate(-0.4,0.2,0);
+  translate(-0.4, 0.2, 0);
   model(DINOOOO);
 }
 
-/*function mousePressed() {
+function mousePressed() {
   if (apeshit.isPlaying()) {
     apeshit.stop();
   } else {
     apeshit.play();
   }
   apeshit.play();
-}*/
+}
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-  DINO = dino2;
-    if (DINO == dino2){
+    if (DINO == dino2) {
       DINO = dino1;
+    } else {
+      DINO = dino2;
     }
   } else if (keyCode === DOWN_ARROW) {
-  DINOO = dino3;
-  }else if (keyCode === LEFT_ARROW) {
-  DINOOO = dino4;
-  }else if (keyCode === RIGHT_ARROW) {
-  DINOOOO = dino5;
+    if (DINOO == dino3) {
+      DINOO = dino1;
+    } else {
+      DINOO = dino3;
+    }
+
+  } else if (keyCode === LEFT_ARROW) {
+    if (DINOOO == dino4) {
+      DINOOO = dino1;
+    } else {
+      DINOOO = dino4;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    if (DINOOOO == dino5) {
+      DINOOOO = dino1;
+    } else {
+      DINOOOO = dino5;
+    }
   }
   return false; // prevent default
 }
